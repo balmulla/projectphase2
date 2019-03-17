6558a6ed82d78967156649b8ed7b6ecb388f0c96
bmulla <bmulla@qatar.cmu.edu>
2019-03-17 18:27:02 +0000

    presence tests for employee pass

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ApplicationRecord
     has_many :assignments
     has_many :stores, through: :assignments 
+    
+    validates :first_name, :last_name, :date_of_birth, :role, :ssn, presence: true
+    
 end
