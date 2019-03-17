6a1198433f50eae5cf91620c773dcba2a0618fb7
bmulla <bmulla@qatar.cmu.edu>
2019-03-17 20:34:58 +0000

    validations for store complete

--- app/models/store.rb
+++ app/models/store.rb
@@ -5,4 +5,10 @@ class Store < ApplicationRecord
     
     #validations
     validates :name, :zip, :street, presence: true
+    validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+    validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+    validates :phone, format: { with: /(^[\d -]+$)/}    
+    validates :zip, format: { with: /\b\d{5}\b/}
+    validates :state, format: { with: /(PA)|(OH)|(WV)/}
+    validates :name, uniqueness: true
 end
