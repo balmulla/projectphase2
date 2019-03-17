cb68814fc7afcfb2bea134a7d23eb271a98a7c2f
bmulla <bmulla@qatar.cmu.edu>
2019-03-17 19:54:55 +0000

    tests for ssn and phone and validations done

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,5 +6,7 @@ class Employee < ApplicationRecord
     #validations
     validates :first_name, :last_name, :date_of_birth, :role, :ssn, presence: true
     validates_datetime :date_of_birth
+    validates :ssn, format: { with: /(\d{3}-\d{2}-\d{4})|(\d{9})/}
+    validates :phone, format: { with: /(^[\d -]+$)/}
     
 end
--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -16,4 +16,17 @@ class EmployeeTest < ActiveSupport::TestCase
   should_not allow_value("string").for(:date_of_birth)
   should_not allow_value(123232).for(:date_of_birth)
   
+  should allow_value("023649320").for(:ssn)
+  should allow_value("023-64-9320").for(:ssn)
+  should_not allow_value("hello").for(:ssn)
+  should_not allow_value("sfs121").for(:ssn)
+  should_not allow_value("121").for(:ssn)
+
+  should allow_value("4122688211").for(:phone)
+  should allow_value("412-268-8211").for(:phone)
+  should_not allow_value("hello").for(:phone)
+  should_not allow_value("412-268_8211").for(:phone)
+  should_not allow_value("412-268h8211").for(:phone)
+  should_not allow_value("412-268$8211").for(:phone)
+  
 end
