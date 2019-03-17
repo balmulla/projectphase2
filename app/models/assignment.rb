bea9098da2829b6cd5267178e9c790b18adb8b7b
bmulla <bmulla@qatar.cmu.edu>
2019-03-17 21:06:30 +0000

    tests for validations complete for all

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,11 @@
 class Assignment < ApplicationRecord
+    #associations
     belongs_to :employee
     belongs_to :store
+    #validations
+    validates :store_id, :employee_id, :start_date, :pay_level,  presence: true
+    validates_datetime :start_date
+    validates_datetime :end_date
+    validates :pay_level, numericality: { only_integer: true }
+    
 end
--- test/models/assignment_test.rb
+++ test/models/assignment_test.rb
@@ -1,6 +1,26 @@
 require 'test_helper'
 
 class AssignmentTest < ActiveSupport::TestCase
+    #Association tests
     should belong_to(:employee)
     should belong_to(:store)
+    
+    #validation tests
+    should validate_presence_of(:store_id) 
+    should validate_presence_of(:employee_id)
+    should validate_presence_of(:start_date) 
+    should validate_presence_of(:pay_level)
+    
+    should allow_value(1.year.ago.to_date).for(:start_date)
+    should allow_value(1.day.ago.to_date).for(:start_date)
+    should_not allow_value("string").for(:start_date)
+    should_not allow_value(123232).for(:start_date)
+    
+    should allow_value(1.year.ago.to_date).for(:end_date)
+    should allow_value(1.day.ago.to_date).for(:end_date)
+    should_not allow_value("string").for(:end_date)
+    should_not allow_value(123232).for(:end_date)
+    
+    should validate_numericality_of(:pay_level)
+    should_not allow_value(1.3).for(:pay_level)
 end
