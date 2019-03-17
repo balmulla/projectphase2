4a3b67b2a26a075bcf6c422a4f1d1312562bb40c
bmulla <bmulla@qatar.cmu.edu>
2019-03-17 18:24:58 +0000

    added presence tests for employee and they fail

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -4,4 +4,11 @@ class EmployeeTest < ActiveSupport::TestCase
   #Association tests
   should have_many(:assignments)
   should have_many(:stores).through(:assignments)
+
+  #Validation tests
+  should validate_presence_of(:first_name) 
+  should validate_presence_of(:last_name)
+  should validate_presence_of(:date_of_birth) 
+  should validate_presence_of(:role)
+  should validate_presence_of(:ssn) 
 end
