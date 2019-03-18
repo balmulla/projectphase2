a08b5ed3a2428f7e554f3258c8f561540f4e10ac
bmulla <bmulla@qatar.cmu.edu>
2019-03-18 20:29:30 +0000

    created factories

--- test/factories/assignments.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :assignment do
-    store_id { 1 }
-    employee_id { 1 }
-    start_date { "2019-03-17" }
-    end_date { "2019-03-17" }
-    pay_level { 1 }
-  end
-end
