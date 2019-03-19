623c9074a327f9d8e6609f8c46eba0d0097bcbfa
bmulla <bmulla@qatar.cmu.edu>
2019-03-19 12:25:41 +0000

    run tests to make sure all is well

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -5,7 +5,7 @@ class Assignment < ApplicationRecord
     #validations
     validates :store_id, :employee_id, :start_date, :pay_level,  presence: true
     validates_datetime :start_date
-    validates_datetime :end_date
+    validates_datetime :end_date, allow_nil: true
     validates :pay_level, numericality: { only_integer: true }
     
 end
